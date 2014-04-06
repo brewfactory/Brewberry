64bd8f9ed503feb32d9200195caeed908dede7bd
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-04-06 08:36:11 +0200

    Adding tests for TempNotifiers

---
+++ test/server/module/Notifiers/TargetTemperatureChanged.js
@@ -0,0 +1,15 @@
+'use strict';
+var TargetTemperatureChanged = require('../../../../module/Notifiers/Temperature/TargetTemperatureChanged');
+var TempEmitterMock = require('../Helpers/EmitterMock');
+
+describe('Notifiers:TargetTemperatureChanged', function () {
+
+  it ('notifies the BrewEmitter', function () {
+    var obj = {};
+    var data = 3;
+    TargetTemperatureChanged(TempEmitterMock(obj), data);
+    expect(obj.eventName).to.be.eql('targetTemperature:set');
+    expect(obj.data).to.be.eql({temp:data});
+  });
+
+});
