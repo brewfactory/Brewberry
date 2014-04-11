f67edb80b2abd165a3bf6d1216c6938ed1822011
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-04-11 11:14:04 +0200

    Remove Helpers

--- test/server/Helpers/EmitterMock.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = function (obj) {
-  return {
-    emit: function(eventName, data) {
-      obj.eventName = eventName;
-      obj.data = data;
-    }
-  };
-};
--- test/server/Helpers/SocketIOMock.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = function (obj) {
-  return {
-    onStatusChange: function (data) {
-      obj.data = data;
-    }
-  };
-};
